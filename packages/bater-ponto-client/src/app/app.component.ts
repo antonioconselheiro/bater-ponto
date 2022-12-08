8de844966ec06b865e30e9e2e6cd4e5f28ebbbd2
António Conselheiro <canudos.sudestino@gmail.com>
2022-12-08 14:24:08 -0300

    criando base para aplicacao client server

---
+++ packages/bater-ponto-client/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bater-ponto-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'bater-ponto-client';
+}
