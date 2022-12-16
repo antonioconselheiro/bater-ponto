cdefa0c22ba690f874a3a3316bdf1dd8c65cf9fb
António Conselheiro <canudos.sudestino@gmail.com>
2022-12-16 14:34:48 +0000

    Revert "instalando e configurando eslint"
    This reverts commit bd6c80418ef2d4297c7948a7d29d4448c843f9d7.

--- packages/bater-ponto-server/src/main.ts
+++ packages/bater-ponto-server/src/main.ts
@@ -8,20 +8,16 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 
-async function bootstrap(): Promise<void> {
+async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   app.enableCors();
-
-  const defaultPort = 3434;
-  const port = process.env.PORT || defaultPort;
+  const port = process.env.PORT || 3434;
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
-
-  return Promise.resolve();
 }
 
-bootstrap().catch(e => console.error(e));
+bootstrap();
