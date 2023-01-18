d42d4aaaa7e65eb9e74d6dd4f0a624de9a54b40b
António Conselheiro <canudos.sudestino@gmail.com>
2023-01-18 15:20:58 +0000

    configurando swagger na api do nestjs

--- packages/bater-ponto-server/src/main.ts
+++ packages/bater-ponto-server/src/main.ts
@@ -5,7 +5,7 @@
 
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
 
 async function bootstrap(): Promise<void> {
@@ -14,6 +14,15 @@ async function bootstrap(): Promise<void> {
   app.setGlobalPrefix(globalPrefix);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Bater ponto')
+    .setDescription('Documentação da api do aplicativo de bater ponto')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
   const defaultPort = 3434;
   const port = process.env.PORT || defaultPort;
